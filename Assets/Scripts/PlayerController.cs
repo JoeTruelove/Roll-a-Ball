5fd15d7a14faa2218c05faae4b315a7a427dd496
JoeTruelove <43154676+JoeTruelove@users.noreply.github.com>
2019-09-05 09:59:39 -0400

    Cleaned up script
    made script a little nicer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,7 +11,7 @@ public class PlayerController : MonoBehaviour
     public Text countText;
     public Text winText;
 
-     void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody>();
         count = 0;
@@ -26,7 +26,7 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement * speed);
     }
-     void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Pick Up"))
         {
